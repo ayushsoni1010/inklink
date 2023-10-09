620f2662815ab180a8dfbd03e083f50c8d947a3e
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-09 06:54:27 +0530

    refactor: layout and index page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "InkLink",
+  title: "inkLink",
   description:
     "A blogging platform that connects writers with their audience through seamless content sharing.",
 };
