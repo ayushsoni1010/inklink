ba6c3cd938f370e35e7db1fab9c65c31f0c5d4bb
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-18 19:45:20 +0530

    chore: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import { BaseButton } from "@/components/base/button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div style={{ margin: "300px" }}>Heyyyyyyyyy</div>;
+  return (
+    <>
+      <div className="grid gap-3 items-center justify-center h-screen">
+        <div className="text-center">
+          {" "}
+          <Button>Website in progress 🚀</Button>
+          <p className="text-sm mt-2 text-center">
+            Checkout{" "}
+            <a href="/login" className="underline underline-offset-4">
+              Login
+            </a>{" "}
+            and{" "}
+            <a href="/signup" className="underline underline-offset-4">
+              Signup
+            </a>{" "}
+            pages
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
