4ac39318153cfba81d8f4b560cc337f65c911550
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-13 23:50:48 +0530

    chore: crafted testimonials data

---
+++ src/data/auth/__testimonialsData.ts
@@ -0,0 +1,26 @@
+export const testimonialsData = [
+  {
+    id: 1,
+    name: "Jack K",
+    title: "Ex Blackrock PM",
+    message:
+      "“Love how Loch integrates portfolio analytics and whale watching into one unified app.”",
+    isLongMessage: false,
+  },
+  {
+    id: 2,
+    name: "Yash P",
+    title: "Research, 3poch Crypto Hedge Fund",
+    message:
+      "“I use Loch everyday now. I don't think I could analyze crypto whale trends markets without it. I'm addicted!”",
+    isLongMessage: false,
+  },
+  {
+    id: 3,
+    name: "Shiv S",
+    title: "Co-Founder Magik Labs",
+    message:
+      "“Managing my own portfolio is helpful and well designed. What’s really interesting is watching the whales though. No one else has made whale tracking so simple.”",
+    isLongMessage: true,
+  },
+];
