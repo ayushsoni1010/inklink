827bac1997f884bcea8933fbed657de436f0d9ca
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-14 02:49:00 +0530

    feat/fix: auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,40 +1,17 @@
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+"use client";
+
+import AuthSidebarLayout from "@/layout/auth";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <section>
-      <div className="container h-screen flex flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            inkLink
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
+      <div className="flex justify-center h-screen">
+        <div className="hidden h-full p-16 text-white dark:border-r lg:block lg:w-7/12 bg-brand-grey-900 overflow-hidden bg-zinc-900">
+          <AuthSidebarLayout />
         </div>
-        {children}
+        <div className="flex items-center justify-center mx-auto">
+          {children}
+        </div>
       </div>
     </section>
   );
