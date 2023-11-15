bc7494022c02c1194b6b4d2d99c981b7f60cef36
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-15 23:03:55 +0530

    perf/enhance: optimize and memoize the performance for icon

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -1,15 +1,18 @@
+import { memo } from "react";
 import dynamic from "next/dynamic";
 import { LucideProps } from "lucide-react";
 import dynamicIconImports from "lucide-react/dynamicIconImports";
 
-interface IconProps extends LucideProps {
+type IconProps = LucideProps & {
   name: keyof typeof dynamicIconImports;
-}
+};
 
-const Icon = ({ name, ...props }: IconProps) => {
+const Icon = memo(({ name, ...props }: IconProps) => {
   const LucideIcon = dynamic(dynamicIconImports[name]);
 
   return <LucideIcon {...props} />;
-};
+});
+
+Icon.displayName = "Icon";
 
 export { Icon };
